49a480a09fb3b2469f87b03690d92af4c839fbd5
Matias Carmona <matias2002carmona@gmail.com>
2023-09-21 23:02:48 -0300

    Agregado de DOM , JSON , LOCAL STORAGE

--- productos.js
+++ productos.js
@@ -1,41 +1,49 @@
 const productos = [{
         id: 1,
         nombre: "REMERA KITTY KITTY",
+        imagen: "img/remerablanca.webp",
         precio: 10000,
     },
     {
         id: 2,
         nombre: "REMERA $INNER",
+        imagen: "img/remeranegra.webp",
         precio: 10000,
     },
     {
         id: 3,
         nombre: "REMERA ROOT",
+        imagen: "img/remera4.png",
         precio: 10000,
     },
     {
         id: 4,
         nombre: "REMERA WASHED",
+        imagen: "img/remerawashed.webp",
         precio: 10000,
     },
     {
         id: 5,
         nombre: "HOODIE BSTRD RENEWED",
+        imagen: "img/buzoblanco.webp",
         precio: 20000,
     },
     {
         id: 6,
         nombre: "HOODIE LOGO 3",
+        imagen: "img/buzonegro.webp",
         precio: 20000,
     },
     {
         id: 7,
         nombre: "HOODIE 13",
+        imagen: "img/buzorosa.webp",
         precio: 20000,
     },
     {
         id: 8,
         nombre: "B$T CARGO",
+        imagen: "img/cargo.webp",
         precio: 15000,
     },
 ];
